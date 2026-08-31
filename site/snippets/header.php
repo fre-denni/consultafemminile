9c2fccf8ae8f1885e29e22911a8089e4ed284984
fre-denni <fredenni.git@gmail.com>
2026-08-31 18:59:02 +0200

    Aggiunto stile e modifiche globali al css nello snippet header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,9 +3,22 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="<?= $site->description() ?>">
+  <meta name="keywords" content="<?= $site->keywords() ?>">
   <title><?= $page->title() ?> | <?= $site->title() ?></title>
   <?= css('assets/css/global.css') ?>
-  <!-- Aggiungi slot per SEO + per stili e javascript personalizzato per template -->
+  <?= css('@auto') ?>   
+  <?= js('@auto') ?>
+  <style>
+    <?php if ($stylesheet = $slots->stylesheet()): ?>
+      <?= $stylesheet ?>
+    <?php endif ?>
+  </style>
+  <script>
+    <?php if ($scripts = $slots->scripts()): ?>
+      <?= $scripts ?>
+    <?php endif ?>
+  </script>
 </head>
 <body>
   <header class="header">
@@ -14,5 +27,8 @@
       <?php foreach($site->children()->listed() as $item): ?>
         <li><a href="<?= $item->url() ?>"><?= $item->title() ?></a></li>
       <?php endforeach ?>
+      <!-- Add search -->
     </nav>
   </header>
+
+<!-- andrebbe un if/else con versione mobile/desktop se non riesco a farlo da me -->

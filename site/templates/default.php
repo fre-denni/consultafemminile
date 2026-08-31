a9b34dcb652b25d54d2c48458b2f5061482b8b0a
fre-denni <fredenni.git@gmail.com>
2026-08-31 18:33:37 +0200

    Test e setup della pagina di default

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="it">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $site->title() ?></title>
-  <?= css('assets/css/global.css') ?>
-  <!-- Aggiungi snippet per SEO + per stili e javascript personalizzato per template -->
-</head>
-<body>
-  
-</body>
-</html>
-
-<h1>Hi!</h1>
+<?php snippet('header') ?>
+  <main>
+  </main>
+<?php snippet('footer') ?>

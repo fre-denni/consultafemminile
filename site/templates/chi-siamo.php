399d4ae4f4b64c2bdf837f89cf58db3fe816a542
fre-denni <fredenni.git@gmail.com>
2026-09-21 00:24:16 +0200

    aggiunto pagina contenitore "chi siamo"

---
+++ site/templates/chi-siamo.php
@@ -0,0 +1,25 @@
+<?php snippet('layout', slots: true) ?>
+
+  <?php slot() ?>
+      <?php snippet('bits/title-page', [
+      'heading' => $site->payoff(),
+      'text'    => $page->mission(),
+      'borderBottom' => true,
+    ]) ?>
+    <?php snippet('atoms/block-container', ['blocks' => $page->text()]) ?>
+    <?php snippet('bits/people-carousel', [
+      'items' => collection('consigliere'),
+      'label' => 'Consiglio 2026-2027',
+      'background' => 'tinted',
+    ]) ?>
+    <?php snippet('bits/people-carousel', [
+      'items' => collection('delegate'),
+      'label' => 'Le delegate delle associazioni',
+    ]) ?>
+    <?php snippet('bits/associazioni-grid', [
+      'items' => collection('associazioni'),
+      'label' => 'Le associazioni',
+    ]) ?>
+  <?php endslot() ?>
+
+<?php endsnippet() ?>

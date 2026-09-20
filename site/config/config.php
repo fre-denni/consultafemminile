5298ab0037c10553f41ee35ef38cd9fecc37bced
fre-denni <fredenni.git@gmail.com>
2026-09-20 20:58:55 +0200

    nuove regole deployment

--- site/config/config.php
+++ site/config/config.php
@@ -1,9 +1,13 @@
 <?php
 
-return [
-  'debug' => true,
-  'extensions' => [
-    'fieldMethods' => require __DIR__ . '/methods.php',
-  ],
-];
+$isStaging = strpos($_SERVER['SCRIPT_NAME'] ?? '', '/staging/') === 0;
 
+return [
+    'debug' => $isStaging,
+    'panel' => [
+        'install' => $isStaging,
+    ],
+    'extensions' => [
+        'fieldMethods' => require __DIR__ . '/methods.php',
+    ],
+];

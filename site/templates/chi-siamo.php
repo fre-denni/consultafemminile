fc5f5e3a6e577b00559e368f9565bb758e94da98
fre-denni <fredenni.git@gmail.com>
2026-09-25 00:44:11 +0200

    aggiunto contact form

--- site/templates/chi-siamo.php
+++ site/templates/chi-siamo.php
@@ -43,7 +43,12 @@ foreach (collection('timeline') as $capitolo) {
       'items' => collection('associazioni'),
       'label' => 'Le associazioni',
     ]) ?>
-
+        <?php snippet('bits/contact-form', [
+      'label'      => $page->etichetta()->or('Contattaci')->value(),
+      'image'      => $page->foto()->toFile(),
+      'credit'     => $page->credit()->value(),
+      'background' => 'tinted',
+    ]) ?>
   <?php endslot() ?>
 
 <?php endsnippet() ?>
